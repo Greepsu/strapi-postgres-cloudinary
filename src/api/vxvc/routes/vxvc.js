13d3d1b5178d83738bf49112da281ef558565218
Greepsu <arnaud.mgn2@gmail.com>
2023-09-12 13:51:57 +0200

    chore: update .strapi-updater.json

---
+++ src/api/vxvc/routes/vxvc.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * vxvc router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::vxvc.vxvc');
---
+++ src/api/vxvc/services/vxvc.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * vxvc service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::vxvc.vxvc');
