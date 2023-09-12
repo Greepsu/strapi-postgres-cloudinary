96e130768eecf18811fcea6b08b26f21b1d30fd4
Greepsu <arnaud.mgn2@gmail.com>
2023-09-12 13:43:30 +0200

    chore: update .strapi-updater.json

---
+++ src/api/sdfsdf/controllers/sdfsdf.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  sdfsdf controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::sdfsdf.sdfsdf');
---
+++ src/api/sdfsdf/services/sdfsdf.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sdfsdf service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::sdfsdf.sdfsdf');
